e98b9533bdf4de3975aa0417fb19a5a5d0a24dfd
Murilo Garcia <murilogarcia.20241@poli.ufrj.br>
2025-09-17 21:23:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
 // Passo A: Configuração do Supabase
 const SUPABASE_URL = 'https://nhiijcgvxxqolnhssmhk.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaWlqY2d2eHhxb2xuaHNzbWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5ODYwNDQsImV4cCI6MjA3MzU2MjA0NH0.zHysnXS6PqU37GRUXDl1Md0RoSpsa6X4V8MI-EMypm0';
