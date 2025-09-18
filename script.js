6b46fb43678c753f54ccdcd9776236ce2b90df93
Murilo Garcia <murilogarcia.20241@poli.ufrj.br>
2025-09-17 22:12:38 -0300

    mudar tema para light

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const initialCoords = [-22.9068, -43.1729];
 const map = L.map('map').setView(initialCoords, 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
 // Função para buscar dados e adicionar ao mapa
