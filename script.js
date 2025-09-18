6b5ac8a23bd2f6cabaf5b04aeb6d544be628295c
Murilo Garcia <murilogarcia.20241@poli.ufrj.br>
2025-09-17 22:17:48 -0300

    tema aquarela

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const initialCoords = [-22.9068, -43.1729];
 const map = L.map('map').setView(initialCoords, 12);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Função para buscar dados e adicionar ao mapa
